506c5d205d0d2bc2cd6c9981de4c2f292a26ca61
JustinPratama <jcwpratama@gmail.com>
2023-04-08 19:20:13 -0400

    changes to app.js and form.jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,49 @@
-import Banner from "./components/Banner"
-import Form from "./components/Form"
-import Quotes from "./components/Quotes"
+import React from 'react';
+import Banner from "./components/Banner";
+import Form from "./components/Form";
+import Quotes from "./components/Quotes";
 import './App.css';
 
 function App() {
+  let total_bmr;
+  function handleFormSubmit(formData) {
+    let name;
+    let age;
+    let unit;
+    let height;
+    let weight;
+    let location;
+    let dietaryRestrictions;
+    let active;
+    let goal;
+
+    console.log(formData);
+    name = formData.name;
+    age = formData.age;
+    unit = formData.unit;
+    height = formData.height;
+    weight = formData.weight;
+    location = formData.location;
+    dietaryRestrictions = formData.dietaryRestrictions;
+    active = formData.active;
+    goal = formData.goal;
+    total_bmr = (10*weight) + (6.25*height) - (5*age) + 5;
+    total_bmr = total_bmr * active;
+    if(goal == "Cut Weight"){
+      total_bmr -= 500;
+    } else if(goal == "Bulk Weight"){
+      total_bmr += 500;
+    } 
+  
+
+
+
+  }
+  
   return (
     <div>
       <Banner/>
-      <Form/>
+      <Form onSubmit={handleFormSubmit}/>
       <Quotes/>
     </div>
   );
