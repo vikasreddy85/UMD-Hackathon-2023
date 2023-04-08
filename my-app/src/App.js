3bd7f1119fccbf74f9fc922c921f57695af468e2
Vikas Reddy <vikas85@terpmail.umd.edu>
2023-04-08 16:17:00 -0400

    update form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,26 +2,15 @@ import React from 'react';
 import Banner from "./components/Banner";
 import Form from "./components/Form";
 import Quotes from "./components/Quotes";
+import { dining } from './components/Constants';
 import './App.css';
 
 function App() {
   function handleFormSubmit(formData) {
     console.log(formData);
+    console.log(dining.food[0].query);
+
     /* query, energy,*/
-    const dining = {
-
-    }
-    
-    const mango = {
-      api_key: 'MPGogOa9dZbgva6VN9LCcgdovtIMdFIDk6Uv4hlO',
-      query: 'mango',
-      dataType: ["Survey (FNDDS)"],
-      pagesize: 1,
-      location: "Y",
-      time: "lunch",
-      dietary: "none",
-    }
-
   }
   
   return (
---
+++ my-app/src/components/Constants.jsx
@@ -0,0 +1,33 @@
+export const Pancakes_Plain = {
+  api_key: 'MPGogOa9dZbgva6VN9LCcgdovtIMdFIDk6Uv4hlO',
+  query: 'Pancakes Plain',
+  dataType: ["Survey (FNDDS)"],
+  pagesize: 1,
+  location: "Y",
+  time: "lunch",
+  dietary: ["dairy", "eggs", "gluten", "soy", "vegetarian"]
+};
+
+export const Scrambled_Cheesy_Cheddar_Egg = {
+  api_key: 'MPGogOa9dZbgva6VN9LCcgdovtIMdFIDk6Uv4hlO',
+  query: 'Scrambled Cheesy Cheddar Egg',
+  dataType: ["Survey (FNDDS)"],
+  pagesize: 1,
+  location: "Y",
+  time: "lunch",
+  dietary: ["dairy", "eggs", "vegetarian"]
+};
+
+export const Shredded_Cheddar_Cheese = {
+  api_key: 'MPGogOa9dZbgva6VN9LCcgdovtIMdFIDk6Uv4hlO',
+  query: 'Shredded Cheddar Cheese',
+  dataType: ["Survey (FNDDS)"],
+  pagesize: 1,
+  location: "Y",
+  time: "lunch",
+  dietary: ["dairy", "vegetarian"]
+};
+
+export const Yahentamitsi_Lunch = {
+  food: [Pancakes_Plain, Scrambled_Cheesy_Cheddar_Egg, Shredded_Cheddar_Cheese]
+};
