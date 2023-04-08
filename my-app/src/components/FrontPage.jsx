7d289bf36b640a542315e733bea9c0120cc37f71
Vikas Reddy <vikas85@terpmail.umd.edu>
2023-04-08 00:43:46 -0400

    started banner

--- my-app/src/components/FrontPage.jsx
+++ my-app/src/components/FrontPage.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import classes from "../components/FrontPage.module.css"
+import React, { useState, useEffect } from 'react';
+import classes from "../components/FrontPage.module.css";
 
 const FrontPage = () => {
-<div className={classes.container}>
-    <div className={classes.banner}>
-        
+  const [keywords, setKeywords] = useState(['Stay in Shape', 'Study Hard', 'Learn More']);
+  const [currentKeywordIndex, setCurrentKeywordIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentKeywordIndex((prevIndex) => (prevIndex + 1) % keywords.length);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [keywords]);
+
+  const currentKeyword = keywords[currentKeywordIndex];
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.banner}>
+        <div className={classes.position}>
+            <div className={classes.image}></div>
+        </div>
+        <div className={classes.text}>
+            <h2>{currentKeyword}</h2>
+        </div>
+      </div>
     </div>
-</div>
+  );
 }
+
 export default FrontPage;
