da78f422e2a763d6c4c1b2efe54fbdd39ec40c4a
Vikas Reddy <vikas85@terpmail.umd.edu>
2023-04-08 18:26:19 -0400

    created json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Banner from "./components/Banner";
 import Form from "./components/Form";
 import Quotes from "./components/Quotes";
-import { Yahentamitsi } from './components/Constants';
+import { Yahentamitsi, South_Diner, North_Diner } from './components/Constants';
 import './App.css';
 
 function App() {
   function handleFormSubmit(formData) {
-    console.log(formData);
     console.log(Yahentamitsi);
-
-    /* query, energy,*/
+    console.log(North_Diner);
+    console.log(South_Diner);
   }
   
   return (
