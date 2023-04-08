2f8b0ab9345c2e18ca71cbc7a381e4073d8a6fba
Vikas Reddy <vikas85@terpmail.umd.edu>
2023-04-07 23:16:05 -0400

    initial commit

--- my-app/src/components/FrontPage.jsx
+++ my-app/src/components/FrontPage.jsx
@@ -1,9 +1,11 @@
-import classes from "./FrontPage.module.css"
+import React from 'react';
+import classes from "../components/FrontPage.module.css"
 
 const FrontPage = () => {
 <div className={classes.container}>
-    
+    <div className={classes.banner}>
         
+    </div>
 </div>
 }
 export default FrontPage;
